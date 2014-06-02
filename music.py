22a9c44ee0f705028f604613a1bca622d8638cac
HarryChen <sdqxcxh@gmail.com>
2014-06-02 16:18:51 +0800

    modify copy function

--- music.py
+++ music.py
@@ -21,8 +21,10 @@ def print_result(word_result):
     """"""
     result_list = result.items()
     result_list = sorted(result_list, key=lambda x:x[1], reverse=True)
+    idx = 0
     for (word, times) in result_list:
-        print word
+        print idx, word
+        idx+=1
 
 def category_file(file_path, dest_dir):
     """"""
@@ -31,16 +33,14 @@ def category_file(file_path, dest_dir):
     dest_path = os.path.join(config.base_dir, dest_dir)
     if not os.path.exists(dest_path):
         try:
-            os.mkdir(config.base_path)
+            os.mkdir(dest_path)
         except Exception , e:
             print "failed to create dir"
             raise e
-    try:
-        shutil.copyfile(file_path, dest_path)
-    except Exception , e:
-        print "failed to copy file"
-        raise e
 
+    copy_cmd = "cp '%s' %s"% (file_path, dest_path)
+    print copy_cmd
+    shutil.copy(filepath, dest_path)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -51,8 +51,22 @@ if __name__ == '__main__':
         except Exception , e:
             print "failed to dected base dir"
 
-        filepath = sys.argv[1]
+        filepath = sys.argv[1].decode("utf-8")
         fpath, fname = os.path.split(filepath)
         filename, ext=os.path.splitext(fname)
         result = parser.parse(filename)
         print_result(result)
+        while True:
+            try:
+                option = int(raw_input("select a most probably one: \n"))
+            except Exception , e:
+                print "Input data error"
+                continue
+            if option >= 0 and option < len(result):
+                try:
+                    category_file(filepath, result.items()[option][0])
+                except Exception , e:
+                    print "failed to copy file to category"
+                    raise e
+                finally:
+                    break
